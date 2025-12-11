58f0f6b3ede949445a25ea85d99418f16b6ea88f
AnhDuy <lehoanhduy5426@gmail.com>
2025-12-11 18:32:41 +0700

    Add files via upload

--- start_dashboard.bat
+++ start_dashboard.bat
@@ -1,32 +1,18 @@
 @echo off
-REM ===========================
-REM 1️⃣ Kết nối ADB với LDPlayer
-REM ===========================
+
 echo [*] Connecting ADB to LDPlayer...
 "C:\LDPlayer\LDPlayer9\adb.exe" connect 127.0.0.1:5555
-if %errorlevel% neq 0 (
-    echo [!] Failed to connect ADB. Check LDPlayer and adb path.
-    pause
-    exit /b
-)
 echo [*] ADB connected!
 
 REM ===========================
-REM 2️⃣ Chạy Flask server
+REM 2️⃣ Activate virtual env + run Flask
 REM ===========================
-cd /d C:\Users\jloy5\OneDrive\Desktop\Rage_of_demonking\bot_project
+cd /d C:\Users\jackw\Desktop\JLOY\Rage_of_demon_king\bot_project
+
+echo [*] Activating venv...
+call venv\Scripts\activate
+
 echo [*] Starting Flask server...
-start python app.py
+python app.py
 
-REM Chờ 3 giây để Flask khởi động
-timeout /t 3
-
-@REM REM ===========================
-@REM REM 3️⃣ (Tùy chọn) Chạy ngrok
-@REM REM ===========================
-@REM REM cd /d C:\ngrok
-@REM REM echo [*] Starting ngrok...
-@REM REM start ngrok http 5000
-
-echo [*] Done! Flask + ADB connected.
 pause
